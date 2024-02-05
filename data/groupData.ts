c3bdf9b69ed698b8e2d8242332ea6b0dd0e75705
zoeeechu <43968711+zoeeechu@users.noreply.github.com>
2024-02-05 13:28:10 -0500

    Added second group

--- data/groupData.ts
+++ data/groupData.ts
@@ -4,6 +4,12 @@ const groupData = [
     description: `A game dev group`,
     imgSrc: '/static/images/bbm.png',
     href: 'https://github.com/Blue-Berry-Muffin/',
+  },
+  {
+    title: 'Shatter bee games',
+    description: `A game dev group`,
+    imgSrc: '/static/images/shatterbee.png',
+    href: 'https://github.com/ShatterBee-Games/',
   }
 ]
 
