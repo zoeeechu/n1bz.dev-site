ad02c4e31a3507a9d8f5134e1748ff666dde13c1
zoeeechu <43968711+zoeeechu@users.noreply.github.com>
2024-02-05 12:53:33 -0500

    Added Groups

---
+++ data/groupData.ts
@@ -0,0 +1,10 @@
+const groupData = [
+  {
+    title: 'Gogoanime Client',
+    description: `Watch your favorite anime from your desktop!`,
+    imgSrc: '/static/images/ako.png',
+    href: 'https://github.com/AkoClient/Ako',
+  }
+]
+
+export default groupData
