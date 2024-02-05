605049c4fd8fdced22160415b7586bb8acba34cd
zoeeechu <43968711+zoeeechu@users.noreply.github.com>
2024-02-05 11:02:55 -0500

    Form page follow site theme

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -1,18 +1,25 @@
 'use client'
 import React from 'react'
 import Giscus from '@giscus/react'
+import { useEffect, useState } from 'react'
+import { useTheme } from 'next-themes'
+import ThemeSwitch from 'components/ThemeSwitch' // Assuming ThemeSwitch component is in a separate file
 
 
 export default function FormPage() {
+  const { theme } = useTheme()
+
   return (
     <div>
-      <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+      <h1 className={`text-3xl font-extrabold leading-9 tracking-tight ${theme === 'dark' ? 'text-gray-100' : 'text-gray-900'} sm:text-4xl sm:leading-10 md:text-6xl md:leading-14`}>
         FormPage
       </h1>
       &nbsp;
-      <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+      <dd className={`text-base font-medium leading-6 ${theme === 'dark' ? 'text-gray-400' : 'text-gray-500'}`}>
         Ask Your Questions Here for Helpful Responses!
       </dd>
+      &nbsp;
+      <ThemeSwitch /> {/* Render the ThemeSwitch component */}
       &nbsp;
       <Giscus
         id="Questions"
@@ -25,7 +32,7 @@ export default function FormPage() {
         reactionsEnabled="1"
         emitMetadata="0"
         inputPosition="bottom"
-        theme="preferred_color_scheme"
+        theme={theme === 'dark' ? 'dark' : 'preferred_color_scheme'} {/* Set the theme based on the current theme */}
         lang="en"
       />
     </div>
