23256db4515d2e30025bc69a4ba090c59b365479
zoeeechu <43968711+zoeeechu@users.noreply.github.com>
2024-12-05 20:05:27 -0500

    Updated About Me

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,5 +1,11 @@
 const projectsData = [
   {
+    title: 'StarStruck Engine',
+    description: `A multipurpose (ECS) based game engine, made from scratch using SDL, OpenGL, C++ and Lua!`,
+    imgSrc: '/static/images/StarStruck_Banner.jpg',
+    href: 'https://github.com/StarStruckEngine/StarStruck',
+  },
+  {
     title: 'Nahla Engine',
     description: `My first game engine, made from scratch using SDL, OpenGL, and C++!`,
     imgSrc: '/static/images/Nahla_Banner-01.png',
