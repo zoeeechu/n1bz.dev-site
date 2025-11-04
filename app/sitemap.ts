ea27f17c163753a41373ed385168084f6ad27834
zoeeechu <43968711+zoeeechu@users.noreply.github.com>
2025-11-04 00:19:55 -0500

    new articles!

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,6 @@
-import { MetadataRoute } from 'next'
-import { allBlogs } from 'contentlayer/generated'
 import siteMetadata from '@/data/siteMetadata'
+import { allBlogs } from 'contentlayer/generated'
+import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const siteUrl = siteMetadata.siteUrl
@@ -9,7 +9,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
     lastModified: post.lastmod || post.date,
   }))
 
-  const routes = ['', 'blog', 'projects', 'tags', 'groups', 'form'].map((route) => ({
+  const routes = ['', 'blog', 'projects', 'tags', 'groups'].map((route) => ({
     url: `${siteUrl}/${route}`,
     lastModified: new Date().toISOString().split('T')[0],
   }))
--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,7 +2,6 @@ const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
-  { href: '/form', title: 'Form' },
   { href: '/projects', title: 'Projects' },
   { href: '/groups', title: 'Groups' },
   { href: '/about', title: 'About' },
