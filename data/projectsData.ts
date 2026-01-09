6a8ecc7f9d38c928c5e9583cf9dbd25a06bc4ccc
zoeeechu <43968711+zoeeechu@users.noreply.github.com>
2026-01-09 03:24:50 -0500

    Revert "cool projects"
    This reverts commit 110a79625c6d59d8d39c3052a0080a7ad582a643.

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,57 +1,15 @@
 const projectsData = [
   {
     title: 'StarStruck Engine',
-    description: `A multipurpose (ECS) based game engine, made from scratch using Vulkan, SDL, OpenGL, C++ and Lua!`,
+    description: `A multipurpose (ECS) based game engine, made from scratch using SDL, OpenGL, C++ and Lua!`,
     imgSrc: '/static/images/StarStruck_Banner.jpg',
     href: 'https://github.com/StarStruckEngine/StarStruck',
-  },
-  {
-    title: 'BuhBuhBuhBubble',
-    description: `A Cute Arcade Style Bubble Game! Made for Global Game Jam 2025`,
-    imgSrc: '/static/images/BUHBUH.png',
-    href: 'https://zoeeechu.itch.io/buhbuhbuhbubbles',
-  },
-  {
-    title: 'PyroCheck',
-    description: `Ever wanted to light your opponents peice on fire well now you can! Made for TOJam 2024`,
-    imgSrc: '/static/images/Pyro.png',
-    href: 'https://zoeeechu.itch.io/pyro-check',
-  },
-  {
-    title: 'Hamstorminator',
-    description: `HAMSTERS... HAMSTERS EVERYWHERE! Made for TOJam 2025`,
-    imgSrc: '/static/images/hamter.png',
-    href: 'https://zoeeechu.itch.io/hamstorminator',
-  },
-  {
-    title: 'Brew Brawl',
-    description: `Brew Brawl, a turn based RPG, with a magical twist!`,
-    imgSrc: '/static/images/BrewB.jpg',
-    href: 'https://shatter-bee.itch.io/brew-brawl',
-  },
-  {
-    title: 'Bean game',
-    description: `First attempt at using Haxe`,
-    imgSrc: '/static/images/Beans.png',
-    href: 'https://zoeeechu.itch.io/bean-game',
-  },
-  {
-    title: 'Planet Simulation',
-    description: `A Physics Based Planet Simulation to determine the best orbit`,
-    imgSrc: '/static/images/planet.gif',
-    href: 'https://www.linkedin.com/feed/update/urn:li:activity:7394488088636559360/',
   },
   {
     title: 'Nahla Engine',
     description: `My first game engine, made from scratch using SDL, OpenGL, and C++!`,
     imgSrc: '/static/images/Nahla_Banner-01.png',
     href: 'https://github.com/zoeeechu/Nahla-Engine',
-  },
-  {
-    title: 'LightLock',
-    description: `A Game Made for LevelUP Showcase People's Choice (3rd)`,
-    imgSrc: '/static/images/LightLock.png',
-    href: 'https://levelupshowcase.com/archive/',
   },
   {
     title: 'Snickers',
